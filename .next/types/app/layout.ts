03645edc525226941edca590b3836c135189f87c
ArdillaV21 <fedevalle21@gmail.com>
2025-03-19 22:53:30 -0300

    add: circulary gallery and valores

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\feduv\Desktop\proto\app\layout.tsx
+// File: C:\Users\feduv\Desktop\portafolio-ginkgo\app\layout.tsx
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
